b9e8542a2e256c95acd24a707f9cef9bd34d627f
kaz-ozy <132529533+kaz-ozy@users.noreply.github.com>
2023-06-19 20:59:26 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         validates :nickname , presence: true
+         validates :email, uniqueness: true
+         validates :encrypted_password, presence: true, length: { minimum: 6 }
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがナマエ全角の正規表現
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがナマエ全角の正規表現
+         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+         validates :birthday, presence: true
+
+         
+
+
+
+
+
+end  
