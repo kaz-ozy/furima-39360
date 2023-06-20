06e3a8c07c31f4268fcce3343bba9bee1e0698c5
kaz-ozy <132529533+kaz-ozy@users.noreply.github.com>
2023-06-20 21:53:25 +0900

    修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname , presence: true
-         validates :email, uniqueness: true
-         validates :encrypted_password, presence: true, length: { minimum: 6 }
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがナマエ全角の正規表現
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがナマエ全角の正規表現
+         
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+         validates :last_name
+         validates :first_name
+          end
+         
+        
          validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
          validates :birthday, presence: true
